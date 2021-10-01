cbdbe963874e30a94c279d80c58fc77e9659c9e6
Juslin Jukka <jusju@haaga-helia.fi>
2021-10-01 09:56:31 +0300

    lisays

--- sql/create.sql
+++ sql/create.sql
@@ -1,17 +1,15 @@
 CREATE TABLE Kayttaja (
-   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   kayttaja_id INTEGER PRIMARY KEY AUTOINCREMENT,
    kayttajatunnus TEXT NOT NULL,
    salasana TEXT NOT NULL
 );
 
-
 CREATE TABLE Kalenterimerkinta (
-   id INTEGER PRIMARY KEY AUTOINCREMENT,
-   tiheys DOUBLE NOT NULL,
-   korkeus DOUBLE NOT NULL,
-   leveys DOUBLE NOT NULL,
-   paino DOUBLE NOT NULL,
-   pituus DOUBLE NOT NULL,
-   grain TEXT NOT NULL
-   
+   kalenterimerkinta_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   alkuaika DATE NOT NULL,
+   loppuaika DOUBLE NOT NULL,
+   kayttaja_id INTEGER NOT NULL,
+  CONSTRAINT fk_kayttajat
+    FOREIGN KEY (kayttaja_id)
+    REFERENCES Kayttaja(kayttaja_id)
 );
