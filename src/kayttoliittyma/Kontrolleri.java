92c300a483c835d7c4eaed4127eb4f7d1bb0c55c
Juslin Jukka <jusju@haaga-helia.fi>
2021-10-01 09:33:20 +0300

    first commit

--- src/kayttoliittyma/Kontrolleri.java
+++ src/kayttoliittyma/Kontrolleri.java
@@ -1,5 +1,16 @@
 package kayttoliittyma;
 
-public class Kontrolleri {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Kontrolleri extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		System.out.println("KONTROLLERISSA");
+	}
 }
